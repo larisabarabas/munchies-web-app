77f51923c420590180eca1151bedaec14995f467
Stefania-Larisa Barabas <stefaniabarabas@gmail.com>
2025-03-24 20:44:15 +0100

    Added the ability to deselect filters

--- store/useFilterStore.ts
+++ store/useFilterStore.ts
@@ -4,7 +4,13 @@ export const useFilterStore = create<State>((set) => ({
     category: null,
     price_range: '',
     delivery_time:null,
-    setCategory: (category) => set({category: category}),
-    setDeliveryTime: (delivery_time) => set({delivery_time: delivery_time}),
-    setPriceRange: (price_range) => set({price_range: price_range})
+    setCategory: (category) =>set((state) => ({
+        category: state.category === category ? null : category,
+    })),
+    setDeliveryTime: (delivery_time) => set((state) => ({
+        delivery_time: state.delivery_time === delivery_time ? null : delivery_time
+    })),
+    setPriceRange: (price_range) => set(((state) => ({
+        price_range: state.price_range === price_range ? '' : price_range
+    })))
 }))
